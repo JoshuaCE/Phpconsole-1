9db4f0c333963be34f21c4baff7e89b23f900240
Dries Vints <dries.vints@gmail.com>
2013-03-06 21:48:38 +0100

    Documentation on unique nickname for users
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- src/config/config.php
+++ src/config/config.php
@@ -8,7 +8,8 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| Add users in an array as show in the example below. You can add as many
-	| users as you like.
+	| users as you like. The nickname will identify a user when sending data to
+	| a user's Phpconsole and must be unique.
 	|
 	*/
 
